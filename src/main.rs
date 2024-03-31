2fa4c15e772d55670d554625c54cebafd5b250a5
Martin Matyášek <martin.matyasek@gmail.com>
2024-03-31 11:01:03 +0200

    Handle multiple PINGs per connection
    Also switch to Tokio.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,72 @@
-use anyhow::{bail, Context, Result};
-
-use std::io::Write;
-use std::net::{TcpListener, TcpStream};
+use anyhow::{Context, Result};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::tcp::WriteHalf;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::task;
 
 const PORT: u16 = 6379;
 
-fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    // NOTE: for now we just ignore the payload and hard-code the response to PING
-    let response = b"+PONG\r\n";
-    stream
-        .write_all(response)
-        .context("failed to write response to PING")?;
-
-    stream.flush().context("failed to flush a response")
+enum Resp {
+    Ping,
 }
 
-fn main() -> Result<()> {
-    println!("Binding to port {PORT}");
-
-    let listener = TcpListener::bind(format!("127.0.0.1:{PORT}")).unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("accepted new connection");
-                handle_connection(stream)?;
-            }
-            Err(e) => bail!("error: {e}"),
-        }
+async fn write_resp(writer: &mut WriteHalf<'_>, resp: Resp) -> Result<()> {
+    match resp {
+        Resp::Ping => writer
+            .write_all(b"+PONG\r\n")
+            .await
+            .context("failed to write response to PING")?,
     }
 
-    Ok(())
+    writer.flush().await.context("failed to flush a response")
+}
+
+async fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let (mut req, mut resp) = stream.split();
+
+    let mut buf = [0; 1024];
+    loop {
+        let n = req.read(&mut buf).await.context("failed to read payload")?;
+
+        if n == 0 {
+            println!("no more bytes left to read, flushing and closing connection");
+            break resp.flush().await.context("failed to flush a response");
+        }
+
+        println!("read {n} bytes from the connection: {:?}", &buf[..n]);
+
+        // NOTE: for now we just ignore the payload and hard-code the response to PING
+        write_resp(&mut resp, Resp::Ping).await?;
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("Binding TCP listener to port {PORT}");
+    let listener = TcpListener::bind(format!("127.0.0.1:{PORT}"))
+        .await
+        .with_context(|| format!("failed to bind to port {PORT}"))?;
+
+    loop {
+        tokio::select! {
+            // signal handling would be here
+
+            conn = listener.accept() => {
+                match conn {
+                    Ok((stream, addr)) => {
+                        println!("accepted new connection at {addr}");
+                        task::spawn(async move {
+                            if let Err(e) = handle_connection(stream).await {
+                                eprintln!("task handling connection failed with {e}");
+                            }
+                        });
+                    },
+                    Err(e) => eprintln!("cannot get client: {e}"),
+                }
+            }
+        }
+
+        // cooperatively yield from the main loop
+        task::yield_now().await;
+    }
 }
