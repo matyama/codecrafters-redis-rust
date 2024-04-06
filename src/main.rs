fdf9587531aaa6a5d2317f48ac53b3a8a5284e96
Martin Matyášek <martin.matyasek@gmail.com>
2024-04-06 18:33:11 +0200

    Configure listening port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env::Args;
 use std::sync::Arc;
 
 use anyhow::{Context, Result};
@@ -6,7 +7,42 @@ use tokio::task;
 
 use redis_starter_rust::{DataReader, DataWriter, Store};
 
-const PORT: u16 = 6379;
+#[derive(Debug)]
+struct Config {
+    port: u16,
+}
+
+impl Default for Config {
+    #[inline]
+    fn default() -> Self {
+        Self { port: 6379 }
+    }
+}
+
+impl TryFrom<Args> for Config {
+    type Error = anyhow::Error;
+
+    fn try_from(args: Args) -> Result<Self> {
+        let mut args = args.into_iter().skip(1);
+
+        let mut cfg = Self::default();
+
+        while let Some(arg) = args.next() {
+            match arg.as_str() {
+                "--port" | "-p" => {
+                    cfg.port = args
+                        .next()
+                        .context("missing argument value for --port")?
+                        .parse()
+                        .context("invalid argument value for --port")?;
+                }
+                _ => continue,
+            }
+        }
+
+        Ok(cfg)
+    }
+}
 
 async fn handle_connection(mut stream: TcpStream, store: Arc<Store>) -> Result<()> {
     let (reader, writer) = stream.split();
@@ -30,10 +66,14 @@ async fn handle_connection(mut stream: TcpStream, store: Arc<Store>) -> Result<(
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("binding TCP listener to port {PORT}");
-    let listener = TcpListener::bind(format!("127.0.0.1:{PORT}"))
+    let Config { port } = std::env::args()
+        .try_into()
+        .context("failed to parse program arguments")?;
+
+    println!("binding TCP listener to port {port}");
+    let listener = TcpListener::bind(format!("127.0.0.1:{port}"))
         .await
-        .with_context(|| format!("failed to bind to port {PORT}"))?;
+        .with_context(|| format!("failed to bind to port {port}"))?;
 
     let store = Arc::new(Store::default());
 
